4bbe2e0579d8c7addb8e65f1d25fb95dbfbc722e
EC2 Default User <ec2-user@ip-172-31-13-33.ec2.internal>
2023-03-16 15:37:34 +0000

    should define your key file

---
+++ main.tf
@@ -0,0 +1,61 @@
+data "aws_ami" "amazon-linux-2" {
+  owners      = ["amazon"]
+  most_recent = true
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "owner-alias"
+    values = ["amazon"]
+  }
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-5.10-hvm*"]
+  }
+}
+
+data "template_file" "userdata" {
+  template = file("${abspath(path.module)}/userdata.sh")
+  vars = {
+    server-name = var.server-name
+  }
+}
+
+resource "aws_security_group" "tf-sec-gr" {
+  name = "${var.tag}-terraform-sec-grp"
+  tags = {
+    Name = var.tag
+  }
+
+  dynamic "ingress" {
+    for_each = var.docker-instance-ports
+    iterator = port
+    content {
+      from_port = port.value
+      to_port = port.value
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  egress {
+    from_port =0
+    protocol = "-1"
+    to_port =0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
